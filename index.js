cbec1d85e51c6a1548db6b2fc1a0bfe66c5c8626
Arima1213 <arima3992@gmail.com>
2023-11-17 23:48:01 +0700

    fix railway

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const router = require('./routers');
-const port = 3000;
+
+require('dotenv').config();
+
+const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
