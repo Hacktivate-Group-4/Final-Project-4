146b8ba1538a0c6be9e84e8c01bbb888d1688811
FIQIH IKHROMIN <FIQIHIKHROMIN@users.noreply.github.com>
2023-12-19 16:32:27 +0700

    perbaruan lagi

--- __test__/comment.test.js
+++ __test__/comment.test.js
@@ -1,28 +1,38 @@
 const request = require("supertest");
 const app = require("../index");
 const { User, Photo, Comment } = require("../models");
-const { createUser, createPhoto, generateTokenTesting, createComment } = require("./testing");
+const {
+  createUser,
+  createPhoto,
+  generateTokenTesting,
+  createComment,
+} = require("./testing");
 
 const dataComment = {
   comment: "commentsTest",
-  PhotoId: 2
+  PhotoId: 2,
+};
+const updateComment = {
+  comment: "commentsTestUpdate",
+  PhotoId: 2,
 };
 
 let token;
 
+// create comments
 describe("POST /comments", () => {
   beforeAll(async () => {
     try {
       const user = await createUser();
       token = await generateTokenTesting(user);
-      await createPhoto(2,"photo 1", user.id);
+      await createPhoto(2, "photo 1", user.id);
     } catch (error) {
       console.error("Error during test setup:", error);
     }
   });
-  it('should create Comments success (201)', (done) => {
+  it("should create Comments success (201)", (done) => {
     request(app)
-      .post('/comments')
+      .post("/comments")
       .send(dataComment)
       .set({ Authorization: token })
       .expect(201)
@@ -32,18 +42,20 @@ describe("POST /comments", () => {
         expect(res.body).toHaveProperty("comment");
         expect(res.body).toHaveProperty("PhotoId");
         expect(res.body).toHaveProperty("UserId");
+        expect(res.body).toHaveProperty("createdAt");
+        expect(res.body).toHaveProperty("updatedAt");
         done();
       });
   });
-  it('should be error no auth (401)', (done) => {
+  it("should be error no auth (401)", (done) => {
     request(app)
-      .post('/comments')
+      .post("/comments")
       .send(dataComment)
-      .set({ Authorization: '' }) 
+      .set({ Authorization: "" })
       .expect(401)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body).toBe('Token not provided!');
+        expect(res.body).toBe("Token not provided!");
         done();
       });
   });
@@ -59,23 +71,24 @@ describe("POST /comments", () => {
   });
 });
 
+// views comments all
 describe("GET /comments", () => {
   beforeAll(async () => {
     try {
       const user = await createUser();
       token = await generateTokenTesting(user);
-      await createPhoto(1,"photo 1", user.id);
-      await createPhoto(2,"photo 2", user.id);
-      await createComment(1, 1, user.id)
-      await createComment(2, 2, user.id)
+      await createPhoto(1, "photo 1", user.id);
+      await createPhoto(2, "photo 2", user.id);
+      await createComment(1, 1, user.id);
+      await createComment(2, 2, user.id);
     } catch (error) {
       console.error("Error during test setup:", error);
     }
   });
-  it('should create Comments success (200)', (done) => {
+  it("should Get Comments success (200)", (done) => {
     request(app)
-      .get('/comments')
-      .set({ Authorization: token }) // Menggunakan format Authorization yang sesuai
+      .get("/comments")
+      .set({ Authorization: token })
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
@@ -87,14 +100,14 @@ describe("GET /comments", () => {
         done();
       });
   });
-  it('should be error no auth (401)', (done) => {
+  it("should be error no auth (401)", (done) => {
     request(app)
-      .get('/comments')
-      .set({ Authorization: '' }) 
+      .get("/comments")
+      .set({ Authorization: "" })
       .expect(401)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body).toBe('Token not provided!');
+        expect(res.body).toBe("Token not provided!");
         done();
       });
   });
@@ -109,23 +122,24 @@ describe("GET /comments", () => {
   });
 });
 
+// views comments by id
 describe("GET /comments/:id", () => {
   beforeAll(async () => {
     try {
       const user = await createUser();
       token = await generateTokenTesting(user);
-      await createPhoto(1,"photo 1", user.id);
-      await createPhoto(2,"photo 2", user.id);
-      await createComment(1, 1, user.id)
-      await createComment(2, 2, user.id)
+      await createPhoto(1, "photo 1", user.id);
+      await createPhoto(2, "photo 2", user.id);
+      await createComment(1, 1, user.id);
+      await createComment(2, 2, user.id);
     } catch (error) {
       console.error("Error during test setup:", error);
     }
   });
-  it('should create Comments success (200)', (done) => {
+  it("should Get Comments success (200)", (done) => {
     request(app)
-      .get('/comments/1')
-      .set({ Authorization: token }) // Menggunakan format Authorization yang sesuai
+      .get("/comments/1")
+      .set({ Authorization: token })
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
@@ -137,14 +151,14 @@ describe("GET /comments/:id", () => {
         done();
       });
   });
-  it('should be error no auth (401)', (done) => {
+  it("should be error no auth (401)", (done) => {
     request(app)
-      .get('/comments')
-      .set({ Authorization: '' }) 
+      .get("/comments")
+      .set({ Authorization: "" })
       .expect(401)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body).toBe('Token not provided!');
+        expect(res.body).toBe("Token not provided!");
         done();
       });
   });
@@ -159,3 +173,131 @@ describe("GET /comments/:id", () => {
   });
 });
 
+// update comments
+describe("PUT /comments/2", () => {
+  beforeAll(async () => {
+    try {
+      const user = await createUser();
+      token = await generateTokenTesting(user);
+      await createPhoto(1, "photo 1", user.id);
+      await createPhoto(2, "photo 2", user.id);
+      await createComment(1, 1, user.id);
+      await createComment(2, 2, user.id);
+    } catch (error) {
+      console.error("Error during test setup:", error);
+    }
+  });
+  it("should update Comments success (200)", (done) => {
+    request(app)
+      .put("/comments/2")
+      .set({ Authorization: token })
+      .expect(200)
+      .send(updateComment)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toHaveProperty("comment", updateComment.comment);
+        expect(res.body).toHaveProperty("PhotoId", updateComment.PhotoId);
+        done();
+      });
+  });
+  // it("should be error not found (404)", (done) => {
+  //   request(app)
+  //     .put("/comments/9")
+  //     .set({ Authorization: token })
+  //     .expect(404)
+  //     .send(updateComment)
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       expect(res.body.message).toBe("Comment with id 9 not found");
+  //       done();
+  //     });
+  //   done();
+  // });
+  it("should be error no auth (401)", (done) => {
+    request(app)
+      .put("/comments/2")
+      .set({ Authorization: "" })
+      .expect(401)
+      .send(updateComment)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toBe("Token not provided!");
+        done();
+      });
+  });
+  afterAll(async () => {
+    try {
+      await User.destroy({ where: {} });
+      await Photo.destroy({ where: {} });
+      await Comment.destroy({ where: {} });
+    } catch (error) {
+      console.error("Error during test cleanup:", error);
+    }
+  });
+});
+
+//delete comments
+describe("DELETE /comments/1", () => {
+  beforeAll(async () => {
+    try {
+      const user = await createUser();
+      token = await generateTokenTesting(user);
+      await createPhoto(1, "photo 1", user.id);
+      await createPhoto(2, "photo 2", user.id);
+      await createComment(1, 1, user.id);
+      await createComment(2, 2, user.id);
+    } catch (error) {
+      console.error("Error during test setup:", error);
+    }
+  });
+  it("should update Comments success (200)", (done) => {
+    request(app)
+      .delete("/comments/1")
+      .set({ Authorization: token })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty(
+          "message",
+          "Comment with id 1 deleted successfully"
+        );
+        done();
+      });
+  });
+  // it('should be error not found (404)', (done) => {
+  //   request(app)
+  //     .delete('/comments/10')
+  //     .set({ Authorization: token })
+  //     .expect(404)
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       expect(res.status).toBe(404);
+  //       expect(res.body && res.body.message).toBe('comment with id 10 not found');
+  //       // expect(res.body).toHaveProperty("message", 'Comment with id 10 not found');
+  //       done();
+  //     });
+  // },10000);
+  // it('should be error no auth (500)', (done) => {
+  //   request(app)
+  //     .delete('/coments/1')
+  //     .set({ Authorization: token })
+  //     // .expect(500)
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       // expect(res.status).toBe(500);
+  //       expect(res.body.message).toBe('Internal Server Error');
+  //       // expect(res.body).toHaveProperty("message",'Internal Server Error');
+  //       done();
+  //     });
+  // });
+  afterAll(async () => {
+    try {
+      await User.destroy({ where: {} });
+      await Photo.destroy({ where: {} });
+      await Comment.destroy({ where: {} });
+    } catch (error) {
+      console.error("Error during test cleanup:", error);
+    }
+  });
+});
--- __test__/social.test.js
+++ __test__/social.test.js
@@ -7,9 +7,14 @@ const dataSocial = {
   name:"sosialTestlagi", 
   social_media_url:"https://github.com/nama_pengguna"
 };
+const updateSocial = {
+  name:"sosialUpdate", 
+  social_media_url:"https://facebook.com/nama_pengguna"
+};
 
 let token;
 
+// create socials
 describe("POST /socials", () => {
   beforeAll(async () => {
     try {
@@ -19,7 +24,7 @@ describe("POST /socials", () => {
       console.error("Error during test setup:", error);
     }
   });
-  it('should create Comments success (201)', (done) => {
+  it('should create Socials success (201)', (done) => {
     request(app)
       .post('/socials')
       .send(dataSocial)
@@ -58,6 +63,8 @@ describe("POST /socials", () => {
   });
 });
 
+
+// views socials all
 describe("GET /socials", () => {
   beforeAll(async () => {
     try {
@@ -69,7 +76,7 @@ describe("GET /socials", () => {
       console.error("Error during test setup:", error);
     }
   });
-  it('should create Comments success (200)', (done) => {
+  it('should create Socials success (200)', (done) => {
     request(app)
       .get('/socials')
       .set({ Authorization: token }) 
@@ -77,7 +84,7 @@ describe("GET /socials", () => {
       .end((err, res) => {
         if (err) return done(err);
         expect(res.body).toHaveLength(2);
-        expect(res.body[0]).toHaveProperty("id ");
+        expect(res.body[0]).toHaveProperty("id");
         expect(res.body[0]).toHaveProperty("name");
         expect(res.body[0]).toHaveProperty("name");
         expect(res.body[0]).toHaveProperty("social_media_url");
@@ -106,6 +113,8 @@ describe("GET /socials", () => {
   });
 });
 
+
+// views socials by id
 describe("GET /socials/:id", () => {
   beforeAll(async () => {
     try {
@@ -118,7 +127,7 @@ describe("GET /socials/:id", () => {
       console.error("Error during test setup:", error);
     }
   });
-  it('should create Comments success (200)', (done) => {
+  it('should view by id Socials success (200)', (done) => {
     request(app)
       .get('/socials/2')
       .set({ Authorization: token }) 
@@ -126,22 +135,34 @@ describe("GET /socials/:id", () => {
       .end((err, res) => {
         if (err) return done(err);
         expect(res.body.id).toBe(2);
-        expect(res.body).toHaveProperty("id ");
+        expect(res.body).toHaveProperty("id");
         expect(res.body).toHaveProperty("name");
         expect(res.body).toHaveProperty("name");
         expect(res.body).toHaveProperty("social_media_url");
         expect(res.body).toHaveProperty("UserId");
         done();
       });
+      done();
   });
   it('should be error no auth (401)', (done) => {
     request(app)
-      .get('/socials/10')
-      .set({ Authorization: token }) 
+      .get('/socials/1')
+      .set({ Authorization: '' }) 
       .expect(401)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body).toBe('user not found!');
+        expect(res.body).toBe('Token not provided!');
+        done();
+      });
+  });
+  it('should be error not found (404)', (done) => {
+    request(app)
+      .get('/socials/10')
+      .set({ Authorization: token }) 
+      .expect(404)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.message).toBe('Not Found');
         done();
       });
   });
@@ -155,3 +176,136 @@ describe("GET /socials/:id", () => {
   });
 });
 
+
+// update socials
+describe("PUT /socials/1", () => {
+  beforeAll(async () => {
+    try {
+      const user = await createUser();
+      token = await generateTokenTesting(user);
+      await createSocial(1, user.id);
+    } catch (error) {
+      console.error("Error during test setup:", error);
+    }
+  });
+  it('should update Socials success (200)', (done) => {
+    request(app)
+      .put('/socials/1')
+      .set({ Authorization: token }) 
+      .expect(200)
+      .send(updateSocial)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body[1][0]).toHaveProperty("name", updateSocial.name);
+        expect(res.body[1][0]).toHaveProperty("social_media_url", updateSocial.social_media_url);
+        expect(res.body[1][0]).toHaveProperty("UserId");
+        done();
+      });
+  });
+  // token null
+  it('should be error no auth (401)', (done) => {
+    request(app)
+      .put('/socials/1')
+      .set({ Authorization: '' }) 
+      .expect(401)
+      .send(updateSocial)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toBe('Token not provided!');
+        done();
+      });
+  });
+  // date not found
+  it('should be error not found (404)', (done) => {
+    request(app)
+      .put('/socials/10')
+      .set({ Authorization: token }) 
+      .expect(404)
+      .send(updateSocial)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.devMessage).toBe('social with id 10 not found');
+        done();
+      });
+  });
+  
+  afterAll(async () => {
+    try {
+      await User.destroy({ where: {} });
+      await SocialMedia.destroy({ where: {} });
+    } catch (error) {
+      console.error("Error during test cleanup:", error);
+    }
+  });
+});
+
+//delete comments
+describe("DELETE /socials/1", () => {
+  beforeAll(async () => {
+    try {
+      const user = await createUser();
+      token = await generateTokenTesting(user);
+      await createSocial(1, user.id);
+      await createSocial(2, user.id);
+      await createSocial(3, user.id);
+    } catch (error) {
+      console.error("Error during test setup:", error);
+    }
+  });
+  it('should delete socials success (200)', (done) => {
+    request(app)
+      .delete('/socials/1')
+      .set({ Authorization: token }) 
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        // expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty("message", 'Data dengan ID 1 berhasil dihapus.');
+        done();
+      });
+  });
+  // token null
+  it('should be error no auth (401)', (done) => {
+    request(app)
+      .delete('/socials/1')
+      .set({ Authorization: '' }) 
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toBe('Token not provided!');
+        done();
+      });
+  });
+  // it('should be error no auth (404)', (done) => {
+  //   request(app)
+  //     .delete('/socials/10')
+  //     .set({ Authorization: token }) 
+  //     .expect(404)
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       expect(res.status).toBe(404);
+  //       // expect(res.body).toHaveProperty('Data not found');
+  //       expect(res.body && res.body.message).toBe('Data dengan ID 10 tidak ditemukan.');
+  //       done();
+  //     });
+  // }, 10000);
+  // it('should be error internal server error (500)', (done) => {
+  //   request(app)
+  //     .delete('/socials/1')
+  //     .set({ Authorization: token }) 
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       // expect(res.status).toBe(500);
+  //       expect(res.body.message).toBe('Internal Server Error');
+  //       done();
+  //     });
+  // });
+  afterAll(async () => {
+    try {
+      await User.destroy({ where: {} });
+      await SocialMedia.destroy({ where: {} });
+    } catch (error) {
+      console.error("Error during test cleanup:", error);
+    }
+  });
+});
